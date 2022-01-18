30d1ca502ba1bf9c237bb5c190bb1ea7f522676f
freedomdebug <7620476+freedomdebug@users.noreply.github.com>
2022-01-18 21:54:26 +0800

    doc: contack-author

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,12 +2,18 @@ module.exports = {
     base: '/',
     // 站点配置
     lang: 'zh-CN',
-    title: 'hi， Tommy ！',
+    title: 'TommyHu',
     description: 'tommyhu.me',
     head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
       logo: '/images/logo.jpg',
+      navbar: [
+        {
+          text: 'contact-author',
+          link: '/bar/contact-author'
+        },
+      ],
     },
   }
