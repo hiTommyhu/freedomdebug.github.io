d539266b7754ed99808fd6ecccfd8dbfdb8be08f
freedomdebug <7620476+freedomdebug@users.noreply.github.com>
2022-01-18 21:27:50 +0800

    feat: init project

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+    base: '/',
+    // 站点配置
+    lang: 'zh-CN',
+    title: 'hi， Tommy ！',
+    description: 'tommyhu.me',
+    head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      logo: '/images/logo.jpg',
+    },
+  }
