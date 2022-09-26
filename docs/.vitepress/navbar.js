e5a1162bced88160728469a1000b68488d0d78e7
tommy.hu <13776119594@163.com>
2022-09-26 15:29:34 +0000

    link

--- docs/.vitepress/navbar.js
+++ docs/.vitepress/navbar.js
@@ -1,5 +1,9 @@
 module.exports = [
     {
+      text: '老相好',
+      link: 'https://tommyhu.cn',
+    },
+    {
       text: '留下口信',
       link: 'https://github.com/freedomdebug/freedomdebug.github.io/issues',
     },
--- docs/index.md
+++ docs/index.md
@@ -12,5 +12,9 @@ hero:
     - theme: brand
       text: 部落格
       link: /files/home.html
+  actions:
+    - theme: alt
+      text: 老相好
+      link: http://tommyhu.cn
 ---
 
